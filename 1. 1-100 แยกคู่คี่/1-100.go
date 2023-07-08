cd82a419b3a117c853d55cc3e6ae1dd57109187f
ROL33T <beeyonger1337@gmail.com>
2023-07-08 09:57:54 +0700

    Update 1-100.go
    เพื่มจำนวนนับ เลขคู่กับคี่ ว่ามีเท่าไรบ้าง

--- 1. 1-100 แยกคู่คี่/1-100.go
+++ 1. 1-100 แยกคู่คี่/1-100.go
@@ -6,15 +6,32 @@ func print(a ...interface{}) {
 	fmt.Println(a...)
 }
 
+func printf(format string, a ...interface{}) {
+	fmt.Printf(format, a...)
+}
+
 func main() {
+	a := 0 //นับเลขคู่
+	b := 0 //นับเลขคี่
+
 	for i := 0; i <= 100; i++ {
 
-		if i%2 == 0 {
+		if i != 0 {
 
-			print("คู่", i)
+			if i%2 == 0 {
+				a++
+				print("คู่", i)
+
+			} else {
+				b++
+				print("คี่", i)
+			}
 
 		} else {
-			print("คี่", i)
+			print("เลข 0 จะไม่นับ")
 		}
+
 	}
+
+	printf("เลขคู่ทั้งหมด %d เลขคี่ทั้งหมด %d", a, b)
 }
