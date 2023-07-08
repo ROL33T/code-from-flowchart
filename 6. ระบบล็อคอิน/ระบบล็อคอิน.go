2dc8b82ed7c7c63a3f905eb5f2c448e3a59e6c33
ROL33T <beeyonger1337@gmail.com>
2023-07-08 22:08:42 +0700

    optimize code
    optimize code

--- 6. ระบบล็อคอิน/ระบบล็อคอิน.go
+++ 6. ระบบล็อคอิน/ระบบล็อคอิน.go
@@ -12,20 +12,10 @@ func print(a ...interface{}) {
 	fmt.Println(a...)
 }
 
-func Scan(a ...interface{}) {
-	fmt.Scan(a...)
-}
-
-func printf(format string, a ...interface{}) {
-	fmt.Printf(format, a...)
-}
 func main() {
-	userinput := ""
-	passwordinput := ""
 	user, password := "test", "1234"
 	countLogin := 0
 	countPass := 0
-	LoginNow := time.Now()
 	expDateLockLogin := time.Now()
 	Cooldown := 30 * time.Minute
 	lockClient := false
@@ -33,90 +23,74 @@ func main() {
 
 	reader := bufio.NewReader(os.Stdin)
 
-	for {
-		LoginNow = time.Now()
+	var userinput, passwordinput string
 
-		if LoginNow.After(expDateLockLogin) {
-			if lockClient {
-				print("ระบบได้ทำการปลดล็อค User ของคุณเรียบร้อยแล้ว")
+	for {
+		if lockClient {
+			diff := expDateLockLogin.Sub(time.Now())
+			days := int(diff.Hours()) / 24
+			hours := int(diff.Hours()) % 24
+			minutes := int(diff.Minutes()) % 60
+			seconds := int(diff.Seconds()) % 60
+
+			durationStr := ""
+
+			if days > 0 {
+				durationStr += fmt.Sprintf("%d วัน ", days)
+			}
+			if hours > 0 {
+				durationStr += fmt.Sprintf("%d ชั่วโมง ", hours)
+			}
+			if minutes > 0 {
+				durationStr += fmt.Sprintf("%d นาที ", minutes)
+			}
+			if seconds > 0 {
+				durationStr += fmt.Sprintf("%d วินาที", seconds)
+			}
+
+			print("ระบบทำการล็อค คุณต้องรอ เวลาถึง", durationStr, "ถึงจะปลดล็อกให้")
+			time.Sleep(1 * time.Second)
+			continue
+		}
+
+		if !isLoginSuccess {
+			print("กรุณาใส่ Username: ")
+			userinput, _ = reader.ReadString('\n')
+			userinput = strings.TrimSpace(userinput)
+
+			print("กรุณาใส่ Password: ")
+			passwordinput, _ = reader.ReadString('\n')
+			passwordinput = strings.TrimSpace(passwordinput)
+
+			if userinput == user && passwordinput == password {
+				print("login ok")
 				countLogin = 0
 				countPass = 0
 				expDateLockLogin = time.Now()
 				lockClient = false
+				isLoginSuccess = true
+				break
+			}
+
+			countLogin++
+			if countLogin >= 10 {
+				expDateLockLogin = time.Now().Add(Cooldown)
+				lockClient = true
 			} else {
-				if !isLoginSuccess {
-					print("กรุณาใส่ Username: ")
-					userinput, _ = reader.ReadString('\r')
-					userinput = strings.TrimSpace(userinput)
-
-					print("กรุณาใส่ Password: ")
-					passwordinput, _ = reader.ReadString('\r')
-					passwordinput = strings.TrimSpace(passwordinput)
-
-					if userinput == user {
-						if passwordinput == password {
-							print("login ok")
-							countLogin = 0
-							countPass = 0
-							expDateLockLogin = time.Now()
-							lockClient = false
-							isLoginSuccess = true
-							// รีเช็ทตัวแปร ทั้งหมด
-							break // ออกลูป
-						} else {
-							countPass++
-							if countPass >= 5 {
-								expDateLockLogin = time.Now().Add(Cooldown)
-								lockClient = true
-							} else {
-								print("รหัสคุณผิดพลาด กรุณาลองใหม่อีกครั้งที่", countPass)
-							}
-						}
-					} else {
-						countLogin++
-						if countLogin >= 10 {
-							expDateLockLogin = time.Now().Add(Cooldown)
-							lockClient = true
-						} else {
-							print("UserName คุณผิดพลาด กรุณาลองใหม่อีกครั้ง", countLogin)
-						}
-					}
+				countPass++
+				if countPass >= 5 {
+					expDateLockLogin = time.Now().Add(Cooldown)
+					lockClient = true
 				} else {
-					// รีเช็ทตัวแปร ทั้งหมด
-					countLogin = 0
-					countPass = 0
-					lockClient = false
-					isLoginSuccess = false
+					print("รหัสคุณผิดพลาด กรุณาลองใหม่อีกครั้งที่", countPass)
 				}
 			}
 		} else {
-
-			if lockClient {
-				diff := expDateLockLogin.Sub(LoginNow)
-				days := int(diff.Hours()) / 24
-				hours := int(diff.Hours()) % 24
-				minutes := int(diff.Minutes()) % 60
-				seconds := int(diff.Seconds()) % 60
-
-				durationStr := ""
-
-				if days > 0 {
-					durationStr += fmt.Sprintf("%d วัน ", days)
-				}
-				if hours > 0 {
-					durationStr += fmt.Sprintf("%d ชั่วโมง ", hours)
-				}
-				if minutes > 0 {
-					durationStr += fmt.Sprintf("%d นาที ", minutes)
-				}
-				if seconds > 0 {
-					durationStr += fmt.Sprintf("%d วินาที", seconds)
-				}
-				print("ระบบทำการล็อค คุณต้องรอ เวลาถึง ", durationStr, " ถึงจะปลดล็อกให้")
-			}
-			time.Sleep(1000 * time.Millisecond)
+			countLogin = 0
+			countPass = 0
+			lockClient = false
+			isLoginSuccess = false
 		}
-
 	}
 
 }
